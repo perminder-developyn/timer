9ab9da95ce6366c728241bf615c3c0daf8181cbf
perminder-developyn <perminder.dhillon@developyn.com>
2022-06-05 23:21:16 +0100

    timer counts down/stops/resets and user can set time

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+<input v-model="updateTimer">
+<button @click="setTime">Set</button>
+<button @click="startTimer">Start</button>
+<button @click="resetTimer">Reset</button>
+<button @click="stopTimer">Stop</button>
+{{timer}}
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+        timer: 60,
+        updateTimer: null,
+        id: ""
+    }
+    },
+    methods:{
+    resetTimer(){
+        window.location.reload()
+    },
+    startTimer(){  
+      this.id = setInterval(() => this.countdownTime(), 1000);
+    },
+    stopTimer(){
+        clearInterval(this.id)
+    },
+    countdownTime(){
+        if(this.timer > 0)
+        this.timer--;
+    },
+    setTime(){
+        this.stopTimer()
+        this.timer = this.updateTimer
+    }
+    },
+    watch: {
+        updateTimer(value){
+            console.log(value)
+        } 
+    }
+
+}
+</script>
+
+
+<style>
+
+</style>
