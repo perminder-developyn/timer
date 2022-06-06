9ac4a1f975dedf20eb6f77bac4f9a78684893fdb
perminder-developyn <perminder.dhillon@developyn.com>
2022-06-06 09:54:46 +0100

    tasks can start countdown

--- src/App.vue
+++ src/App.vue
@@ -16,54 +16,66 @@
             <button>Add Todo</button>
         </form>
     <ul>
+        <p v-if="hasStarted">
+            {{timer}} seconds remaining to complete {{currentTask.task}}
+        </p>
     <li v-for="todo in tasks" :key="todo.time">
-        {{todo.time}} Seconds to complete {{ todo.task}}
+        {{todo.timer}} Seconds to : {{ todo.task}}
         <button @click="removeTodo(todo)">X</button>
+        <button @click="startTask(todo)">Begin Task</button>
     </li>
     </ul>
 </template>
 
 <script>
-export default{
-    data(){
-        return{
+export default {
+    data() {
+        return {
         timer: 60,
         updateTimer: null,
         id: "",
         task: '',
         tasks: [],
-
+        currentTask: {},
+        hasStarted: false
         }
     },
-    methods:{
-      addTodo() {
-        this.tasks.push({ time: this.time, task: this.task, done: false })
-        this.task, this.time = '';
+    methods: {
+    startTask(task) {
+        this.currentTask = task;
+        this.hasStarted = true;
+        this.timer = this.currentTask.timer;
+        this.startTimer();
+    },
+    addTodo() {
+        this.tasks.push({ timer: this.time, task: this.task, done: false });
+        this.task="";
+        this.time = "";
     },
     removeTodo(task) {
-        this.tasks = this.tasks.filter((t) => t !== task)
+        this.tasks = this.tasks.filter((t) => t !== task);
     },
-    resetTimer(){
-        window.location.reload()
+    resetTimer() {
+        window.location.reload();
     },
-    startTimer(){  
+    startTimer() {  
         this.id = setInterval(() => this.countdownTime(), 1000);
     },
-    stopTimer(){
-        clearInterval(this.id)
+    stopTimer() {
+        clearInterval(this.id);
     },
-    countdownTime(){
-        if(this.timer > 0)
+    countdownTime() {
+        if (this.timer > 0)
         this.timer--;
     },
     setTime(){
-        this.stopTimer()
-        this.timer = this.updateTimer
+        this.stopTimer();
+        this.timer = this.updateTimer;
     }
     },
     watch: {
-        updateTimer(value){
-            console.log(value)
+        updateTimer(value) {
+            console.log(value);
         } 
     }
 
