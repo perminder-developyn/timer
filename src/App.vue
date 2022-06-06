16dc119983ce6eff64fbe7d2c916b9d9a41921f4
perminder-developyn <perminder.dhillon@developyn.com>
2022-06-06 11:49:23 +0100

    syntax

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,36 @@
 <template>
-    <h1 v-if='tasks.length > 0'>
+    <h1 v-if="tasks.length > 0">
         You have {{tasks.length}} tasks to complete
     </h1>
     <h1 v-else>
         Please enter a task and time to begin
     </h1>
 
-    <button @click='resetTimer'>Reset</button>
-    <button @click='stopTimer'>Stop</button>
-    <button @click='startTimer'>Start</button>
+    <button @click="resetTimer">Reset</button>
+    <button @click="stopTimer">Stop</button>
+    <button @click="startTimer">Start</button>
 
-    <input v-model='task' placeholder='Set Task'>
-    <input v-model='time' placeholder='Set Time'>
+    <input v-model="task" placeholder="Set Task">
+    <input v-model="time" placeholder="Set Time">
 
-    <button @click='addTodo'>Add Todo</button>
-    <p v-if='tryAgain'>please enter a time and task</p>
+    <button @click="addTodo">Add Todo</button>
+    <p v-if="tryAgain">please enter a time and task</p>
 
-    <h1 v-if='timer > 0'>
+    <h1 v-if="timer > 0">
         {{timer}} seconds remaining to complete {{currentTask.task}}
     </h1>
     <h1 v-else>
         Begin a task
     </h1>
     <ul>
-        <li v-for='todo in tasks' :key='todo.time'>
-            <h2 v-if='todo.timer > 0'>
-            {{todo.timer}} Seconds to : {{ todo.task}}
-            <button @click='removeTodo(todo)'>X</button>
-            <button @click='startTask(todo)'>Begin Task</button>
-            </h2>
+        <li v-for="todo in tasks" :key="todo.time">
+            <div v-if="todo.timer > 0">
+                <h2>
+                    {{todo.timer}} Seconds to : {{todo.task}}
+                </h2>
+                <button @click="removeTodo(todo)">X</button>
+                <button @click="startTask(todo)">Begin Task</button>
+            </div>
         </li>
     </ul>
 </template>
